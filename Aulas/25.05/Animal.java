07226a88e3cfd548447055dd55dae7dd3d3bc5a3
Emilly Lautert dos Santos <emilly.santos@atitus.edu.br>
2026-05-25 19:40:08 -0300

    Reorganização das pastas

---
+++ Aulas/25.05/Animal.java
@@ -0,0 +1,26 @@
+package 25.05;
+
+public class Animal {
+    private String nome;
+    private String especie;
+    private Integer idade;
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEspecie() {
+        return especie;
+    }
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+    public Integer getIdade() {
+        return idade;
+    }
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    } 
+}
